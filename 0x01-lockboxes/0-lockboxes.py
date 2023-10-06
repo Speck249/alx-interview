df5001e9902e246166785d25be4021b25e654670
Rediet <redietkfle@gmail.com>
2023-10-05 21:11:16 -0700

    Pycodestyle update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,22 +3,22 @@
 def canUnlockAll(boxes):
     num_boxes = len(boxes)
     unlocked = [False] * num_boxes
-    # Track whether each box is unlocked
+    """Track whether each box is unlocked"""
     unlocked[0] = True
-    # The first box is already unlocked
+    """The first box is already unlocked"""
 
     stack = [0]
-    # Start with the first box
+    """Start with first box"""
     while stack:
         current_box = stack.pop()
-        # Get the current box from the stack
+        """Get the current box from the stack"""
         for key in boxes[current_box]:
-            # Check keys in the current box
+            """Check keys in the current box"""
             if key < num_boxes and not unlocked[key]:
                 unlocked[key] = True
-                # Mark the box as unlocked
+                """Mark the box as unlocked"""
                 stack.append(key)
-                # Add box to stack for further exploration
+                """Add box to stack for further exploration"""
 
     return all(unlocked)
-    # Return True if all boxes are unlocked, False otherwise
+    """Return True if all boxes are unlocked, False otherwise"""
