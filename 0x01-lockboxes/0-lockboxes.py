92e708af1818fb846165df3d341a62cfd7f97400
Rediet <redietkfle@gmail.com>
2023-10-05 23:03:32 -0700

    Determine if locked boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
+    """
+    Method determines if n number of 
+    locked boxes can be opened.
+    """
     num_boxes = len(boxes)
     unlocked = [False] * num_boxes
-    """Track whether each box is unlocked"""
     unlocked[0] = True
-    """The first box is already unlocked"""
 
     stack = [0]
-    """Start with first box"""
     while stack:
         current_box = stack.pop()
-        """Get the current box from the stack"""
         for key in boxes[current_box]:
-            """Check keys in the current box"""
             if key < num_boxes and not unlocked[key]:
                 unlocked[key] = True
-                """Mark the box as unlocked"""
                 stack.append(key)
-                """Add box to stack for further exploration"""
 
     return all(unlocked)
-    """Return True if all boxes are unlocked, False otherwise"""
