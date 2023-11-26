7572c67bcfbc6be206751cafea2c69c35c02b43b
Rediet <redietkfle@gmail.com>
2023-11-26 09:14:28 -0800

    Fix styling issue

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,14 +4,15 @@ Module determines fewest coins
 needed to meet a given amount total
 """
 
+
 def makeChange(coins, total):
-  if total <= 0:
-    return 0
-  min_coins = [float('inf')] * (total + 1)
-  min_coins[0] = 0
-  
-  for coin in coins:
-    for value in range(coin, total + 1):
-      min_coins[value] = min(min_coins[value], min_coins[value - coin] + 1)
-  
-  return min_coins[total] if min_coins[total] != float('inf') else -1
+    if total <= 0:
+        return 0
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for coin in coins:
+        for val in range(coin, total + 1):
+            min_coins[val] = min(min_coins[val], min_coins[val - coin] + 1)
+
+    return min_coins[total] if min_coins[total] != float('inf') else -1
