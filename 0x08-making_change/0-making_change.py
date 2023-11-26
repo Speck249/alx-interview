84eb6116f9f7da4ec855608523fd00874c19b4ab
Rediet <redietkfle@gmail.com>
2023-11-26 09:05:38 -0800

    Make a change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+"""
+Module determines fewest coins
+needed to meet given amount total
+"""
+
+def makeChange(coins, total):
+  if total <= 0:
+    return 0
+  min_coins = [float('inf')] * (total + 1)
+  min_coins[0] = 0
+  
+  for coin in coins:
+    for value in range(coin, total + 1):
+      min_coins[value] = min(min_coins[value], min_coins[value - coin] + 1)
+  
+  return min_coins[total] if min_coins[total] != float('inf') else -1
