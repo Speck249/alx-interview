308c6816140530bcde43fc6691bb8f1cac1789ee
Rediet <redietkfle@gmail.com>
2023-12-02 02:24:06 -0800

    Validate UTF encoding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Determine if given data set
+represents valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Validate UTF encoding
+    """
+    num_bytes = 0
+
+    for num in data:
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & num:
+                num_bytes += 1
+                mask >>= 1
+            
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+            if num_bytes == 0:
+                continue
+
+        else:
+            if not (num >> 6 == 0b10):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
