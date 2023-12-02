b1cb9a10b5e0b547f6c5fa46c1e23573fe33b6ad
Rediet <redietkfle@gmail.com>
2023-12-02 02:20:21 -0800

    Return Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,24 +6,27 @@ the given Grid.
 
 
 def island_perimeter(grid):
-    if not grid:
-        return 0
+    """
+    Method calculates and returns
+    Perimeter o given grid.
+    """
+    perimeter = 0
 
     rows = len(grid)
     cols = len(grid[0])
 
-    perimeter = 0
-
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if i == rows - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
 
     return perimeter
